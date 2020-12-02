10177fab09c594fb085984f488ac15b6ff5f8141
victoriaehrbar <victoriaehrbar@gmail.com>
2020-12-02 03:02:38 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,11 +3,11 @@ require 'pry'
 class String
 
   def sentence?
-      self.end_with?(".")
-    end
+    self.end_with?(".")
+  end
 
   def question?
-
+    self.end_with?(".")
   end
 
   def exclamation?
